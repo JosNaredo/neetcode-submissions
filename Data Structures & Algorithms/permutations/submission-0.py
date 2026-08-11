5ad190fb6d758ac31ac8038051b3cdb11c5b9354
JosNaredo <102762487+JosNaredo@users.noreply.github.com>
2026-08-11 14:50:22 -0600

    Add: permutations - submission-0

---
+++ Data Structures & Algorithms/permutations/submission-0.py
@@ -0,0 +1,16 @@
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        permutation = []
+        def permutate(n, a):
+            if a not in permutation:
+                permutation.append(a[:])
+                # return
+            for i in range(n):
+                permutate(n - 1, a)
+                if n % 2 == 0:
+                    a[i], a[n-1] = a[n-1], a[i]
+                else:
+                    a[0], a[n-1] = a[n-1], a[0]
+
+        permutate(len(nums), nums)
+        return permutation
